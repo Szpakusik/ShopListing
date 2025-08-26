3ee7e96826dc17bbd793d74c539d2500572707d0
szpakusik <szpakusik@gmail.com>
2025-08-26 14:31:07 +0200

    Review changes

--- shop-listing/src/components/TopNavigation.tsx
+++ shop-listing/src/components/TopNavigation.tsx
@@ -1,5 +1,5 @@
-import './TopNavigation.css'
-import logo from '../assets/logo.svg'
+import "./TopNavigation.css";
+import logo from "../assets/logo.svg";
 
 export function TopNavigation() {
   return (
@@ -9,13 +9,18 @@ export function TopNavigation() {
           <img src={logo} className="brand__icon" alt="" />
           <span className="brand__name">ShopOnline</span>
         </div>
-        <div className="search">
-          <span className="search__icon" />
-          <input className="search__input" placeholder="Search for products" />
+        <div className="search" role="search">
+          <span className="search__icon" aria-hidden="true" />
+          <input
+            className="search__input"
+            type="search"
+            placeholder="Search for products"
+            aria-label="Search for products"
+            enterKeyHint="search"
+            name="q"
+          />
         </div>
       </div>
     </header>
-  )
+  );
 }
-
-
